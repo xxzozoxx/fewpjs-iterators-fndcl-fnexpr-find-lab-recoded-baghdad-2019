f94c964e714d1b456c081ed27d86d462786e326f
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-29 18:06:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(arr) {
+  let findWin = arr.find(function(w){
+     return w['result'] === 'W';})
+  if(findWin === undefined){
+    return findWin;
+  }
+  else{
+ return findWin['year'];
+  }
+  
 }
